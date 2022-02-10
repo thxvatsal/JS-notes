e903baa9d0e5a72e8403d908463e853c3341362a
vatsal-boy <95976661+vatsal-boy@users.noreply.github.com>
2022-02-10 16:17:06 +0530

    Create for-sumupto100.js

---
+++ Day 2/For-loops Examples/for-sumupto100.js
@@ -0,0 +1,11 @@
+// program to find sum of all numbers from 1 to 100 
+
+// declaring a sum variable where the sum will be stored
+let sum = 0
+
+// using for loop
+for (let i = 1; i <=100; i++) {
+    sum+=i // adding i to the sum
+}
+// logging the sum value
+console.log(sum)
