b4ff6d5821d3f873703f8db28548add155785a18
Aqib Nawaz <aqibnawaz032@gmail.com>
2022-02-12 08:31:38 +0500

    updated script.js
    updated script.js file, added some details to make it easier to understand. Added an additional code performing the same functionality but is in shorter format

--- Day 5/Random-Number-Generator/script.js
+++ Day 5/Random-Number-Generator/script.js
@@ -1,14 +1,41 @@
 // Read -> Realise -> Write -> Reshape
 
 // Think twice -> Code Once
+// Read -> Realise -> Write -> Reshape
 
-const number = document.querySelector('.count-number');
+// Think twice -> Code Once
 
-const btn = document.querySelector('.generate');
+/*
+======================================================
+======================================================
+//creating variables
+
+const number = document.querySelector('.rand-numbers');
+const btn = document.querySelector('.randNum-gen-btn');
+
+//Adding click event to the button
 
 btn.addEventListener('click' , generateNumber);
+
+//Function to be performed when someone clicks on the button.
 
 function generateNumber () {
     const rand = Math.floor(Math.random() * 1000 + 1);
     number.innerHTML = rand;
 }
+
+
+======================================================
+======================================================
+*/
+
+
+/*
+==================================================================
+You can also write the above commented code in more shorter format
+==================================================================
+*/
+
+document.querySelector('.randNum-gen-btn').addEventListener('click' , () => {
+   document.querySelector('.rand-numbers').innerHTML = Math.floor(Math.random() * 1000 + 1);
+});
